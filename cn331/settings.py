dbde3b5a36b1a3773014ae53156282013771849f
Chonlasit Mooncorn <6110613020@student.tu.ac.th>
2021-10-02 23:20:27 +0700

    change db to postgres

--- cn331/settings.py
+++ cn331/settings.py
@@ -76,15 +76,15 @@ WSGI_APPLICATION = 'cn331.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-#
 '''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -95,7 +95,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-'''
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
